6b86687b268cc2b4e60c9fe51883b6ef4dd983de
Robert Fryman <robert.fryman@gmail.com>
2020-06-26 22:20:05 -0500

    not sure the wording of how the results should be shown

--- coding_challenge_1.rb
+++ coding_challenge_1.rb
@@ -1,10 +1,10 @@
-p 'Jim has four seven-sided dice, each with faces numbered 1, 2, 3, 4, 5, 6, 7.'
-p 'Jill has seven four-sided dice, each with faces numbered 1, 2, 3, 4.'
+# Jim has four seven-sided dice, each with faces numbered 1, 2, 3, 4, 5, 6, 7.
+# Jill has seven four-sided dice, each with faces numbered 1, 2, 3, 4.
 #
-p 'Both of them roll all of their dice and calculate the sums of the numbers on their respective dice. The one with the higher sum wins.'
+# Both of them roll all of their dice and calculate the sums of the numbers on their respective dice. The one with the higher sum wins.
 #
 # QUESTION:
-p 'What is the probability that Jim wins? Give your answer rounded to seven decimal places.'
+# What is the probability that Jim wins? Give your answer rounded to seven decimal places.
 
 
 jim_dice  = *(1..7)
@@ -21,4 +21,7 @@ jim_possibilities.each do |jim|
   end
 end
 
+print "#{jim_win}/#{jim_win+jill_win_or_draw}\nor \n"
 p jim_win.fdiv(jim_win + jill_win_or_draw).round(7)
+print "or \n"
+puts "#{jim_win.fdiv(jim_win + jill_win_or_draw).round(9)*100}%"
